2e1c8e6081c866f71d2658a7001c6ecbffaba74e
Mohd Aqib <aqib@logicwind.com>
2021-05-31 23:56:33 +0530

    added addTask componenet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -23,6 +24,12 @@ function App() {
           reminder: false
       }
   ])
+  //Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
 
   //Delete Task
   const deleteTask = (id) => {
@@ -37,6 +44,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ?<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />: "No task found"}
     </div>
   );
